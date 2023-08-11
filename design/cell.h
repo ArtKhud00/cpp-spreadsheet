969a78a130b01db901948e2b1381e34bcd424d1e
ArtKhud00 <artem.hudyakov2000@gmail.com>
2023-08-11 18:24:03 +0300

    design first commit

--- design/cell.h
+++ design/cell.h
@@ -2,6 +2,7 @@
 
 #include "common.h"
 #include "formula.h"
+#include <unordered_set>
 
 class Cell : public CellInterface {
 public:
@@ -15,9 +16,44 @@ public:
     std::string GetText() const override;
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
+    //можете воспользоваться нашей подсказкой, но это необязательно.
+    class Impl {
+    public:   
+        virtual Value GetValue() const = 0;
+        virtual std::string GetText() const = 0;
+        virtual ~Impl() = default;
+        };
+
+    class EmptyImpl : public Impl {
+    public:
+        Value GetValue() const override;
+        std::string GetText() const override;
+    };
+
+    class TextImpl : public Impl {
+    public:
+        explicit TextImpl(std::string expression);
+
+        Value GetValue() const override;
+        std::string GetText() const override;
+
+    private:
+        std::string text_;
+    };
+
+    class FormulaImpl : public Impl{
+    public:
+        explicit FormulaImpl(std::string expression);
+
+        Value GetValue() const override;
+        std::string GetText() const override;
+
+    private:
+        std::unique_ptr<FormulaInterface> formula_;
+    };
+
     std::unique_ptr<Impl> impl_;
+    Sheet& sheet_;
+    std::unordered_set<Cell*> dependent_cells_;
+    
 };
